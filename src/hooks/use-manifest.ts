5d7cabab52bc24b43d579f042320d680facd3ef2
Marco Borchi <borchimarco@gmail.com>
2021-10-01 19:44:07 +0200

    add options params to useAppManifest hook

--- src/hooks/use-manifest.ts
+++ src/hooks/use-manifest.ts
@@ -1,15 +1,15 @@
 import {useState, useEffect} from 'react';
 import {MicroApp} from '../types';
 
-const useAppManifest = (path = 'manifest.json'): [MicroApp[], boolean] => {
+const useAppManifest = (
+    path = 'manifest.json',
+    options: RequestInit = {method: 'GET', mode: 'no-cors'},
+): [MicroApp[], boolean] => {
     const [microapps, setMicroapps] = useState<MicroApp[]>([]);
     const [loadError, setLoadError] = useState<boolean>(false);
 
     useEffect(() => {
-        fetch(path, {
-            method: 'GET',
-            mode: 'no-cors',
-        })
+        fetch(path, options)
             .then((res) => res.json())
             .then((apps: MicroApp[]) => {
                 setMicroapps(apps);
